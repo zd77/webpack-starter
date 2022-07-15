fb7865ab8b478f4115ff63b17efed53f43370a96
Dereck Z <zavaladereck77@gmail.com>
2022-07-14 23:26:23 -0600

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+let nombre = 'Dereck';
+saludar( nombre );
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output: {
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html'
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns : [
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+}
